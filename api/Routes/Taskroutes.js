debd81ccc0f74055471e32034de819727aac435c
Supratim-Pathak <supratimpathak@gmail.com>
2024-10-11 09:50:44 +0530

    ==

--- api/Routes/Taskroutes.js
+++ api/Routes/Taskroutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const route = express.Router();
 const taskControllers = require("../Controllers/TaskController");
 
-const PREFIX = "task";
+const PREFIX ="task"
 
-route.get(`${PREFIX}/task-view`, taskControllers.ReadAll);
+route.get(`/${PREFIX}/task-view`, taskControllers.ReadAll);
 route.delete(`/${PREFIX}/delete`, taskControllers.ReadAll);
 route.get(`/${PREFIX}/:id`, taskControllers.ReadOne);
 route.put(`/${PREFIX}/update`, taskControllers.ReadAll);
